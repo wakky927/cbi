531b9627d5c2d1d66af15599873beebc27e12288
Takuya Wada <42827146+wakky927@users.noreply.github.com>
2022-02-15 08:48:26 +0900

    Add pre.py

---
+++ bin/pre_2022_02_07_2_c.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+cd ../src/2022_02_07/
+
+python3 pre.py "c" "/media/lfc/ボリューム/M1/result/2022_02_08/pre/sub/_1/" "/media/lfc/ボリューム/M1/result/2022_02_08/pre/calib/_1/" 0 3234 &
+python3 pre.py "c" "/media/lfc/ボリューム/M1/result/2022_02_08/pre/sub/_2/" "/media/lfc/ボリューム/M1/result/2022_02_08/pre/calib/_2/" 0 3234 &
+python3 pre.py "c" "/media/lfc/ボリューム/M1/result/2022_02_08/pre/sub/_3/" "/media/lfc/ボリューム/M1/result/2022_02_08/pre/calib/_3/" 0 3234 &
+python3 pre.py "c" "/media/lfc/ボリューム/M1/result/2022_02_08/pre/sub/_4/" "/media/lfc/ボリューム/M1/result/2022_02_08/pre/calib/_4/" 0 3234 &
+python3 pre.py "c" "/media/lfc/ボリューム/M1/result/2022_02_08/pre/sub/_5/" "/media/lfc/ボリューム/M1/result/2022_02_08/pre/calib/_5/" 0 3234 &
+python3 pre.py "c" "/media/lfc/ボリューム/M1/result/2022_02_08/pre/sub/_6/" "/media/lfc/ボリューム/M1/result/2022_02_08/pre/calib/_6/" 0 3234 &
+python3 pre.py "c" "/media/lfc/ボリューム/M1/result/2022_02_08/pre/sub/_7/" "/media/lfc/ボリューム/M1/result/2022_02_08/pre/calib/_7/" 0 3234 &
+python3 pre.py "c" "/media/lfc/ボリューム/M1/result/2022_02_08/pre/sub/_8/" "/media/lfc/ボリューム/M1/result/2022_02_08/pre/calib/_8/" 0 3234 &
+python3 pre.py "c" "/media/lfc/ボリューム/M1/result/2022_02_08/pre/sub/_9/" "/media/lfc/ボリューム/M1/result/2022_02_08/pre/calib/_9/" 0 3234 &
+python3 pre.py "c" "/media/lfc/ボリューム/M1/result/2022_02_08/pre/sub/_10/" "/media/lfc/ボリューム/M1/result/2022_02_08/pre/calib/_10/" 0 3234 &
+python3 pre.py "c" "/media/lfc/ボリューム/M1/result/2022_02_08/pre/sub/_11/" "/media/lfc/ボリューム/M1/result/2022_02_08/pre/calib/_11/" 0 3234 &
+python3 pre.py "c" "/media/lfc/ボリューム/M1/result/2022_02_08/pre/sub/_12/" "/media/lfc/ボリューム/M1/result/2022_02_08/pre/calib/_12/" 0 3234 &
+python3 pre.py "c" "/media/lfc/ボリューム/M1/result/2022_02_08/pre/sub/_13/" "/media/lfc/ボリューム/M1/result/2022_02_08/pre/calib/_13/" 0 3234 &
+python3 pre.py "c" "/media/lfc/ボリューム/M1/result/2022_02_08/pre/sub/_14/" "/media/lfc/ボリューム/M1/result/2022_02_08/pre/calib/_14/" 0 3234 &
+python3 pre.py "c" "/media/lfc/ボリューム/M1/result/2022_02_08/pre/sub/_15/" "/media/lfc/ボリューム/M1/result/2022_02_08/pre/calib/_15/" 0 3234 &
+python3 pre.py "c" "/media/lfc/ボリューム/M1/result/2022_02_08/pre/sub/_16/" "/media/lfc/ボリューム/M1/result/2022_02_08/pre/calib/_16/" 0 3234 &
+python3 pre.py "c" "/media/lfc/ボリューム/M1/result/2022_02_08/pre/sub/_17/" "/media/lfc/ボリューム/M1/result/2022_02_08/pre/calib/_17/" 0 3234 &
+python3 pre.py "c" "/media/lfc/ボリューム/M1/result/2022_02_08/pre/sub/_18/" "/media/lfc/ボリューム/M1/result/2022_02_08/pre/calib/_18/" 0 3234 &
+python3 pre.py "c" "/media/lfc/ボリューム/M1/result/2022_02_08/pre/sub/_19/" "/media/lfc/ボリューム/M1/result/2022_02_08/pre/calib/_19/" 0 3234 &
+python3 pre.py "c" "/media/lfc/ボリューム/M1/result/2022_02_08/pre/sub/_20/" "/media/lfc/ボリューム/M1/result/2022_02_08/pre/calib/_20/" 0 3234 &
+python3 pre.py "c" "/media/lfc/ボリューム/M1/result/2022_02_08/pre/sub/_22/" "/media/lfc/ボリューム/M1/result/2022_02_08/pre/calib/_22/" 0 3234 &
+python3 pre.py "c" "/media/lfc/ボリューム/M1/result/2022_02_08/pre/sub/_23/" "/media/lfc/ボリューム/M1/result/2022_02_08/pre/calib/_23/" 0 3234 &
+python3 pre.py "c" "/media/lfc/ボリューム/M1/result/2022_02_08/pre/sub/_24/" "/media/lfc/ボリューム/M1/result/2022_02_08/pre/calib/_24/" 0 3234 &
+python3 pre.py "c" "/media/lfc/ボリューム/M1/result/2022_02_08/pre/sub/_25/" "/media/lfc/ボリューム/M1/result/2022_02_08/pre/calib/_25/" 0 3234 &
+python3 pre.py "c" "/media/lfc/ボリューム/M1/result/2022_02_08/pre/sub/_26/" "/media/lfc/ボリューム/M1/result/2022_02_08/pre/calib/_26/" 0 3234 &
+python3 pre.py "c" "/media/lfc/ボリューム/M1/result/2022_02_08/pre/sub/_27/" "/media/lfc/ボリューム/M1/result/2022_02_08/pre/calib/_27/" 0 3234 &
+python3 pre.py "c" "/media/lfc/ボリューム/M1/result/2022_02_08/pre/sub/_28/" "/media/lfc/ボリューム/M1/result/2022_02_08/pre/calib/_28/" 0 3234 &
+python3 pre.py "c" "/media/lfc/ボリューム/M1/result/2022_02_08/pre/sub/_29/" "/media/lfc/ボリューム/M1/result/2022_02_08/pre/calib/_29/" 0 3234 &
+python3 pre.py "c" "/media/lfc/ボリューム/M1/result/2022_02_08/pre/sub/_30/" "/media/lfc/ボリューム/M1/result/2022_02_08/pre/calib/_30/" 0 3234 &
+python3 pre.py "c" "/media/lfc/ボリューム/M1/result/2022_02_08/pre/sub/_31/" "/media/lfc/ボリューム/M1/result/2022_02_08/pre/calib/_31/" 0 3234 &
+python3 pre.py "c" "/media/lfc/ボリューム/M1/result/2022_02_08/pre/sub/_32/" "/media/lfc/ボリューム/M1/result/2022_02_08/pre/calib/_32/" 0 3234 &
+python3 pre.py "c" "/media/lfc/ボリューム/M1/result/2022_02_08/pre/sub/_33/" "/media/lfc/ボリューム/M1/result/2022_02_08/pre/calib/_33/" 0 3234 &
+python3 pre.py "c" "/media/lfc/ボリューム/M1/result/2022_02_08/pre/sub/_34/" "/media/lfc/ボリューム/M1/result/2022_02_08/pre/calib/_34/" 0 3234 &
+python3 pre.py "c" "/media/lfc/ボリューム/M1/result/2022_02_08/pre/sub/_35/" "/media/lfc/ボリューム/M1/result/2022_02_08/pre/calib/_35/" 0 3234 &
+python3 pre.py "c" "/media/lfc/ボリューム/M1/result/2022_02_08/pre/sub/_36/" "/media/lfc/ボリューム/M1/result/2022_02_08/pre/calib/_36/" 0 3234 &
+python3 pre.py "c" "/media/lfc/ボリューム/M1/result/2022_02_08/pre/sub/_37/" "/media/lfc/ボリューム/M1/result/2022_02_08/pre/calib/_37/" 0 3234 &
+python3 pre.py "c" "/media/lfc/ボリューム/M1/result/2022_02_08/pre/sub/_38/" "/media/lfc/ボリューム/M1/result/2022_02_08/pre/calib/_38/" 0 3234 &
+python3 pre.py "c" "/media/lfc/ボリューム/M1/result/2022_02_08/pre/sub/_39/" "/media/lfc/ボリューム/M1/result/2022_02_08/pre/calib/_39/" 0 3234 &
+python3 pre.py "c" "/media/lfc/ボリューム/M1/result/2022_02_08/pre/sub/_40/" "/media/lfc/ボリューム/M1/result/2022_02_08/pre/calib/_40/" 0 3234 &
+python3 pre.py "c" "/media/lfc/ボリューム/M1/result/2022_02_08/pre/sub/_41/" "/media/lfc/ボリューム/M1/result/2022_02_08/pre/calib/_41/" 0 3234 &
+python3 pre.py "c" "/media/lfc/ボリューム/M1/result/2022_02_08/pre/sub/_42/" "/media/lfc/ボリューム/M1/result/2022_02_08/pre/calib/_42/" 0 3234 &
+python3 pre.py "c" "/media/lfc/ボリューム/M1/result/2022_02_08/pre/sub/_43/" "/media/lfc/ボリューム/M1/result/2022_02_08/pre/calib/_43/" 0 3234 &
+python3 pre.py "c" "/media/lfc/ボリューム/M1/result/2022_02_08/pre/sub/_44/" "/media/lfc/ボリューム/M1/result/2022_02_08/pre/calib/_44/" 0 3234 &
+python3 pre.py "c" "/media/lfc/ボリューム/M1/result/2022_02_08/pre/sub/_45/" "/media/lfc/ボリューム/M1/result/2022_02_08/pre/calib/_45/" 0 3234 &
+python3 pre.py "c" "/media/lfc/ボリューム/M1/result/2022_02_08/pre/sub/_46/" "/media/lfc/ボリューム/M1/result/2022_02_08/pre/calib/_46/" 0 3234 &
+
+wait
+
+echo "all program fin."
