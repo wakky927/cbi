87ff668e483f5cac76d0a4e43d46c1a83ea2f6b9
lfc <wakky.927.5831@gmail.com>
2022-02-25 18:52:18 +0900

    piyo

--- src/2022_02_08/modules/calib.py
+++ src/2022_02_08/modules/calib.py
@@ -2,9 +2,9 @@ import numpy as np
 
 
 def px2mm(data):
-    length = 0
-    x0, y0 = 0, 0
-    x1, y1 = 0, 0
+    length = 22
+    x0, y0 = 507, 181
+    x1, y1 = 567, 181
     alpha = length / np.sqrt((x0 - x1)**2 + (y0 - y1)**2)
 
     return data * alpha
