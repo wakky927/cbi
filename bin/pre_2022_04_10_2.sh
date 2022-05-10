0bf2f08ea7891c518bc18ad8e7acfe9face34008
Takuya Wada <42827146+wakky927@users.noreply.github.com>
2022-05-10 14:21:14 +0900

    Create pre_2022_04_10_2.sh
    from github

---
+++ bin/pre_2022_04_10_2.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+cd ../src/2022_04_10/
+
+#python3 pre_light_2.py "C001H001S0001" &
+#python3 pre_light_2.py "C001H001S0002" &
+#python3 pre_light_2.py "C001H001S0003" &
+#python3 pre_light_2.py "C001H001S0004" &
+#python3 pre_light_2.py "C001H001S0005" &
+#python3 pre_light_2.py "C001H001S0006" &
+
+wait
+
+python3 pre_light_2.py "C001H001S0007" &
+python3 pre_light_2.py "C001H001S0008" &
+python3 pre_light_2.py "C001H001S0009" &
+python3 pre_light_2.py "C001H001S0010" &
+
+wait
+
+echo "all program fin."
