74f5d1724f224ab071e8111d0ce594846ea954ac
phi <loblium@gmail.com>
2022-05-13 14:35:45 +0900

    add dark mode in pre-processing.

---
+++ bin/pre_2022_04_09_dark.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+cd ../src/2022_04_09/
+
+python3 pre_dark.py "G:\M2" "C001H001S0001" &
+python3 pre_dark.py "G:\M2" "C001H001S0002" &
+python3 pre_dark.py "G:\M2" "C001H001S0003" &
+python3 pre_dark.py "G:\M2" "C001H001S0004" &
+python3 pre_dark.py "G:\M2" "C001H001S0005" &
+python3 pre_dark.py "G:\M2" "C001H001S0006" &
+python3 pre_dark.py "G:\M2" "C001H001S0007" &
+python3 pre_dark.py "G:\M2" "C001H001S0008" &
+python3 pre_dark.py "G:\M2" "C001H001S0009" &
+python3 pre_dark.py "G:\M2" "C001H001S0010" &
+
+wait
+
+echo "all program fin."
